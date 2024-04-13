8336bacabfcf58c99c559c753399d538f6769535
lyazj <seeson@pku.edu.cn>
2024-04-13 12:15:57 +0800

    Update plotting.

--- plot/plot_atmo.py
+++ plot/plot_atmo.py
@@ -10,17 +10,25 @@ events = uproot.open('../data/O16_1,999.ntuple.root:tree').arrays()
 
 neutrino_logE_min = np.log(ak.min(events['neutrino_p4','fE']))
 neutrino_logE_max = np.log(ak.max(events['neutrino_p4','fE']))
-neutrino_E_bin = np.logspace(neutrino_logE_min, neutrino_logE_max, 51, base=np.e)
+neutrino_E_bin = np.logspace(neutrino_logE_min, neutrino_logE_max, 41, base=np.e)
 
 lepton_logE_min = np.log(ak.min(events['lepton_p4','fE']))
 lepton_logE_max = np.log(ak.max(events['lepton_p4','fE']))
-lepton_E_bin = np.logspace(lepton_logE_min, lepton_logE_max, 51, base=np.e)
+lepton_E_bin = np.logspace(lepton_logE_min, lepton_logE_max, 41, base=np.e)
 
 cos_theta_min = 0
 cos_theta_max = 1
-cos_theta_bin = np.linspace(cos_theta_min, cos_theta_max)
+cos_theta_bin = np.linspace(cos_theta_min, cos_theta_max, 41)
+
+latex = {
+    11: r'{e^-}',     -11: r'{e^+}',
+    12: r'{\nu_e}',   -12: r'{\bar\nu_e}',
+    13: r'{\mu^-}',   -13: r'{\mu^+}',
+    14: r'{\nu_\mu}', -14: r'{\bar\nu_\mu}',
+}
 
 for neutrino_pid in [12, -12, 14, -14]:
+    lepton_pid = neutrino_pid - np.sign(neutrino_pid)
     cutted_events = events[events['neutrino_pid'] == neutrino_pid]
     print('Neutrino %d: %d/%d events' % (neutrino_pid, len(cutted_events['weight']), len(events['weight'])))
 
@@ -41,7 +49,7 @@ for neutrino_pid in [12, -12, 14, -14]:
     plt.hist2d(neutrino_cos_theta.to_numpy(), neutrino_E.to_numpy(), (cos_theta_bin, neutrino_E_bin),
                weights=weight.to_numpy(), norm=mpl.colors.LogNorm())
     plt.xlabel(r'$\cos\theta$')
-    plt.ylabel(r'$E_\nu$ [GeV]')
+    plt.ylabel(f'$E_{latex[neutrino_pid]}$ [GeV]')
     plt.yscale('log')
     plt.colorbar()
     plt.tight_layout()
@@ -52,8 +60,8 @@ for neutrino_pid in [12, -12, 14, -14]:
     plt.hist2d(lepton_cos_theta.to_numpy(), lepton_E.to_numpy(), (cos_theta_bin, lepton_E_bin),
                weights=weight.to_numpy(), norm=mpl.colors.LogNorm())
     plt.xlabel(r'$\cos\theta$')
-    plt.ylabel(r'$E_\mu$ [GeV]')
+    plt.ylabel(f'$E_{latex[lepton_pid]}$ [GeV]')
     plt.yscale('log')
     plt.colorbar()
     plt.tight_layout()
-    plt.savefig('plot_lepton_%d.pdf' % (neutrino_pid - np.sign(neutrino_pid)))
+    plt.savefig('plot_lepton_%d.pdf' % lepton_pid)
